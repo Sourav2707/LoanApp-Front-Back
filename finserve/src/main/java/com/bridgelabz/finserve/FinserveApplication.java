3fcbb1093fb62cb7b5a81d255090a3a2a52592a9
Sourav2707 <smartsourav33@gmail.com>
2023-10-23 01:57:53 +0530

    Changes made to remove .git to avoid submodule

--- finserve/src/main/java/com/bridgelabz/finserve/FinserveApplication.java
+++ finserve/src/main/java/com/bridgelabz/finserve/FinserveApplication.java
@@ -10,4 +10,7 @@ public class FinserveApplication {
 	}
 
 }
+
+
+//spring_output_ansi_enabled=always;spring_profiles_active=default;url=jdbc:mysql://localhost:3306/bridgelabzfinserve?createDatabaseIfNotExist=True;userName=root;password=root;email=smartsourav33@gmail.com;email-password=etxtfpdqgbhkrfwl
 //spring_security_user=sourav;spring_security_password=So@123456
